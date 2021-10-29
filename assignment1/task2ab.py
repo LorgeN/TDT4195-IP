033ed205c4e3c6835a4498e3a3c13b6bb04a64ad
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-10-29 12:48:31 +0200

    Assignment progress

--- assignment1/task2ab.py
+++ assignment1/task2ab.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pathlib
+import numpy as np
 from utils import read_im, save_im
+
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
 
@@ -10,7 +12,7 @@ plt.imshow(im)
 
 
 def greyscale(im):
-    """ Converts an RGB image to greyscale
+    """Converts an RGB image to greyscale
 
     Args:
         im ([type]): [np.array of shape [H, W, 3]]
@@ -19,7 +21,7 @@ def greyscale(im):
         im ([type]): [np.array of shape [H, W]]
     """
 
-    return im
+    return im[:, :, 0] * 0.212 + im[:, :, 1] * 0.7152 + im[:, :, 2] * 0.0722
 
 
 im_greyscale = greyscale(im)
@@ -28,7 +30,7 @@ plt.imshow(im_greyscale, cmap="gray")
 
 
 def inverse(im):
-    """ Finds the inverse of the greyscale image
+    """Finds the inverse of the greyscale image
 
     Args:
         im ([type]): [np.array of shape [H, W]]
@@ -37,4 +39,9 @@ def inverse(im):
         im ([type]): [np.array of shape [H, W]]
     """
     # YOUR CODE HERE
-    return im
+    return np.full_like(im, 1) - im
+
+
+im_inverse = inverse(im_greyscale)
+save_im(output_dir.joinpath("lake_inverse.jpg"), im_inverse, cmap="gray")
+plt.imshow(im_inverse, cmap="gray")
--- assignment1/task2c.py
+++ assignment1/task2c.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pathlib
 import numpy as np
 from utils import read_im, save_im, normalize
+
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
 
@@ -10,9 +11,11 @@ im = read_im(pathlib.Path("images", "lake.jpg"))
 plt.imshow(im)
 
 
-def convolve_im(im, kernel,
-                ):
-    """ A function that convolves im with kernel
+def convolve_im(
+    im,
+    kernel,
+):
+    """A function that convolves im with kernel
 
     Args:
         im ([type]): [np.array of shape [H, W, 3]]
@@ -23,23 +26,49 @@ def convolve_im(im, kernel,
     """
     assert len(im.shape) == 3
 
-    return im
+    k = len(kernel)
+    k2 = k // 2
+    H, W, c = im.shape
+
+    # Recreate an array padded with 0s so we dont have to worry about index out
+    # of bounds at later stages
+    src = np.zeros(np.array([H + k, W + k, c]))
+    # Copy the image given as input into the center of our padded array
+    src[k2 : (H + k2), k2 : (W + k2), :] = im
+
+    # Create a new numpy array in the shape of the image to use as a result value
+    res = np.zeros_like(im)
+
+    for y in range(H):
+        for x in range(W):
+            # Extract the section of the source image that we need to multiply
+            # with our kernel. Extracted flipped so we can do a convolution by
+            # multiplying with our kernel. Alternatively we could flip the kernel.
+            pix = src[(y + k) : y : -1, (x + k) : x : -1, :]
+
+            # Iterate over the R, G and B components
+            for i in range(c):
+                res[y, x, i] = (pix[:, :, i] * kernel).sum()
+
+    return res
 
 
 if __name__ == "__main__":
     # Define the convolutional kernels
-    h_b = 1 / 256 * np.array([
-        [1, 4, 6, 4, 1],
-        [4, 16, 24, 16, 4],
-        [6, 24, 36, 24, 6],
-        [4, 16, 24, 16, 4],
-        [1, 4, 6, 4, 1]
-    ])
-    sobel_x = np.array([
-        [-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1]
-    ])
+    h_b = (
+        1
+        / 256
+        * np.array(
+            [
+                [1, 4, 6, 4, 1],
+                [4, 16, 24, 16, 4],
+                [6, 24, 36, 24, 6],
+                [4, 16, 24, 16, 4],
+                [1, 4, 6, 4, 1],
+            ]
+        )
+    )
+    sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 
     # Convolve images
     im_smoothed = convolve_im(im.copy(), h_b)
@@ -48,12 +77,18 @@ if __name__ == "__main__":
     save_im(output_dir.joinpath("im_sobel.jpg"), im_sobel)
 
     # DO NOT CHANGE. Checking that your function returns as expected
-    assert isinstance(
-        im_smoothed, np.ndarray),         f"Your convolve function has to return a np.array. " + f"Was: {type(im_smoothed)}"
-    assert im_smoothed.shape == im.shape,         f"Expected smoothed im ({im_smoothed.shape}" + \
-        f"to have same shape as im ({im.shape})"
-    assert im_sobel.shape == im.shape,         f"Expected smoothed im ({im_sobel.shape}" + \
-        f"to have same shape as im ({im.shape})"
+    assert isinstance(im_smoothed, np.ndarray), (
+        f"Your convolve function has to return a np.array. "
+        + f"Was: {type(im_smoothed)}"
+    )
+    assert im_smoothed.shape == im.shape, (
+        f"Expected smoothed im ({im_smoothed.shape}"
+        + f"to have same shape as im ({im.shape})"
+    )
+    assert im_sobel.shape == im.shape, (
+        f"Expected smoothed im ({im_sobel.shape}"
+        + f"to have same shape as im ({im.shape})"
+    )
     plt.subplot(1, 2, 1)
     plt.imshow(normalize(im_smoothed))
 
